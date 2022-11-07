3216f6748620186e3e657079158ddd9742b41ab8
Meldo-Megimi <67182853+Meldo-Megimi@users.noreply.github.com>
2022-11-07 04:16:52 +0100

    - bump Version number

--- PageScrobbler.user.js
+++ PageScrobbler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EH – Page Scrobbler
 // @namespace    https://github.com/Meldo-Megimi/EH-Page-Scrobbler/raw/main/PageScrobbler.user.js
-// @version      2022.11.07.03
+// @version      2022.11.07.04
 // @description  Visualize GID and add the ability to easily jump or scrobble
 // @author       FabulousCupcake, OsenTen, Qserty, Meldo-Megimi
 // @license      MIT
