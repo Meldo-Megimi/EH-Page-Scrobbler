b18fcb63082b56e12fe6d4426bdbfdee22b2e12b
FooIbar <118464521+FooIbar@users.noreply.github.com>
2022-11-23 21:58:19 +0800

    Fix mousemove on bar-hovertext

--- PageScrobbler.user.js
+++ PageScrobbler.user.js
@@ -583,6 +583,7 @@ ${yearDiv}
 
             const maxGID = localStorage.getItem("EHPS-maxGID");
             const width = e.target.clientWidth;
+            if (width == 0) return;
             const hoverGID = ((1.0 - offset / width) * maxGID).toFixed(0);
 
             const url = new URL(location.href);
